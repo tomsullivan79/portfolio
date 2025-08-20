23205c8036bfcae29077c36a8f2792a57a4909ee
Tom Sullivan <tsullivan79@gmail.com>
2025-08-19 21:54:03 -0500

    baseline working layout & styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: { plugins: [tailwind()] },
+});
